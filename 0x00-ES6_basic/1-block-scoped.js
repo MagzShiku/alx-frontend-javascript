6d11e7a7ac8d3aa94e08e5859a3ac9ebfb23889f
Ubuntu <ubuntu@ip-172-31-19-71.eu-north-1.compute.internal>
2024-02-05 11:36:10 +0000

    task 1_rev01

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-	let task = false;
-	let task2 = true;
+	const task = false;
+	const task2 = true;
 
 	if (trueOrFalse) {
 		//If the input parameter 'trueOrFalse' is true, the variables 'task' and 'task2' are redeclared and assigned with 'true' and 'false' respectively inside the conditional block.
-		let task = true;
-		let task2 = false;
+		const task = true;
+		const task2 = false;
 	}
 	return [task, task2];
 }
